f29dc951c7400156385037183e2836dea692a0c5
Oleh Baliuk <oleh.baliuk@nixsolutions.com>
2023-02-24 14:08:16 +0200

    init commit

---
+++ src/views/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <Header />
+        <div>123</div>
+    </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+import Header from '@/components/Header.vue';
+
+@Component({
+    components: {
+        Header,
+    },
+})
+export default class MainPage extends Vue {}
+</script>
